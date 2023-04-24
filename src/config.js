6e51eb960664c677d965791ee6b7a6fbcc431ab0
Juan Daniel Muñoz Dueñas <88680309+DanielMunoz4190@users.noreply.github.com>
2023-04-24 16:41:06 -0600

    ajustes

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import {config} from 'dotenv';
 config();
 
 export default {
-    host: process.env.HOST || "aws.connect.psdb.cloud",
-    database: process.env.DATABASE || "kueski",
-    user:  "u35vxa8sxvoifmh6w0rf",
-    password: process.env.PASSWORD || "pscale_pw_AVdxd81bnTCoz8GWZEIHSjOzibAVIbumfWPyXl4Y3oJ"
+    host: process.env.HOST || "",
+    database: process.env.DATABASE || "",
+    user:  process.env.USERNAME || "",
+    password: process.env.PASSWORD || ""
 };
